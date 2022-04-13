4a610c2b4f73bc6b0e7201fa4e7b64138d5ce2bd
Алексей Сарвилин <a.sarvilin@yandex.ru>
2022-04-14 00:19:38 +0300

    api_final_yatube_iter6

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,5 +3,5 @@ from rest_framework import permissions
 
 class IsAuthorChangeDeleteOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS or
-                obj.author == request.user)
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
