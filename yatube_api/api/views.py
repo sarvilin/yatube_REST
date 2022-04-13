b0e55947707ad3f49d3992b5a578d30267b4ad74
Алексей Сарвилин <a.sarvilin@yandex.ru>
2022-04-13 17:44:29 +0300

    api_final_yatube_iter3

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, filters
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 
 from api.permissions import IsAuthorChangeDeleteOnly
 from api.serializers import (FollowSerializer, PostSerializer,
